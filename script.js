47bf6229713aee1fbd2ef990be0d57ac1e3bb982
Jailton Aragão <jailton.aragao@gmail.com>
2022-09-04 21:49:20 -0300

    Projeto Relogio Smart conectado Api de tempo Finaliado.

--- script.js
+++ script.js
@@ -55,3 +55,106 @@ function moveRelogio() {
 
   setTimeout("moveRelogio()", 1000)
 }
+
+// Função pegandpo data
+function pegarData() {
+
+  // pegando dados do metodo p/ variaveis
+  let diaDaSemana = dataHora.getDay()
+  let dia = dataHora.getDate()
+  let mes = dataHora.getMonth() + 1
+  let ano = dataHora.getFullYear()
+
+  // Convertendo number em string para formatar texto
+  let strDia = new String(dia)
+  let strMes = new String(mes)
+
+  //Concatenando com + 0
+  if (strDia.length == 1) dia = '0' + dia
+  if (strMes.length == 1) mes = '0' + mes
+
+  // Setando o dia da semana com o switch
+  switch (diaDaSemana) {
+    case 0:
+      diaDaSemana = 'DOM'
+      break;
+    case 1:
+      diaDaSemana = 'SEG'
+      break;
+    case 2:
+      diaDaSemana = 'TER'
+      break;
+    case 3:
+      diaDaSemana = 'QUA'
+      break;
+    case 4:
+      diaDaSemana = 'QUI'
+      break;
+    case 5:
+      diaDaSemana = 'SEX'
+      break;
+    case 6:
+      diaDaSemana = 'SÁB'
+      break;
+  }
+
+  let dataAtual = dia + '/' + mes + '/' + ano
+
+  semana.textContent = diaDaSemana
+  data.textContent = dataAtual
+}
+// Evocando a função
+pegarData()
+
+// Analizando tipo de formataçao dos dados
+var options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+}
+// Variavel de teste com Date
+let teste = new Date()
+
+// toLocaleString para fortar numero
+console.log(teste.toLocaleString('pt-BR'))
+// Teste com dados formatados pela variavel option
+console.log(teste.toLocaleString('pt-BR', options))
+console.log(teste.toLocaleDateString('pt-BR'))
+console.log(teste.toLocaleTimeString('pt-BR'))
+
+//Conectando API de clima tempo
+function getUserPosition() {
+  let url = ''
+  navigator.geolocation.getCurrentPosition((pos) => {
+    let lat = pos.coords.latitude
+    let long = pos.coords.longitude
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=c4866228915c658730601ed7036cd6ac`
+    fetchApi(url)
+    console.log(url)
+  })
+}
+
+function fetchApi(url) {
+  let city = document.querySelector('.city')
+  let temperature = document.querySelector('#temp')
+  let humidity = document.querySelector('#umidad')
+
+  fetch(url)
+    .then((data) => {
+      return data.json()
+    })
+    .then((data) => {
+      let tempInCelsius = ((5 / 9) * (data.main.temp - 32)).toFixed(1);
+
+      city.textContent = data.name
+      temperature.innerHTML = tempInCelsius
+      humidity.innerHTML = data.main.humidity
+    })
+    .catch((err) => {
+      city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
+      temperature.innerHTML = `-`;
+    })
+}
+
+getUserPosition();
